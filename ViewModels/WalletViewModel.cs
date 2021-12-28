ee9232129e14dadfbb1f218d55bcd0bd7f277f06
kika <kikamozga18@ya.ru>
2021-12-28 13:06:57 +0300

    добавлена viewmodel для кошельков

---
+++ ViewModels/WalletViewModel.cs
@@ -0,0 +1,21 @@
+using System.Collections.ObjectModel;
+using ReactiveUI;
+using MoneyApp.Models;
+
+namespace MoneyApp.ViewModels
+{
+    public class WalletViewModel : ViewModelBase
+    {
+        private ObservableCollection<WalletAdapter>? _walletAdpters;
+
+        public ObservableCollection<WalletAdapter> WalletAdapters 
+        {
+            get => _walletAdpters!;
+            set => this.RaiseAndSetIfChanged(ref _walletAdpters, value);
+        }
+
+        public WalletViewModel(){
+
+        }
+    }
+}
