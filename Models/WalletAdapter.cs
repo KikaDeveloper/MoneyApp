6e39f30c201e0765a2e94920afff73bf9c6cbb8e
kika <kikamozga18@ya.ru>
2021-12-28 13:08:03 +0300

    добавлен класс объединения кошелька и view model

---
+++ Models/WalletAdapter.cs
@@ -0,0 +1,10 @@
+using MoneyApp.ViewModels;
+
+namespace MoneyApp.Models
+{
+    public class WalletAdapter
+    {
+        public Wallet Wallet { get; set; }
+        public CategoryManagerViewModel ViewModel { get; set; }
+    }   
+}
