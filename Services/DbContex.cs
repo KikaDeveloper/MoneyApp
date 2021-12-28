e944ff0b625b03582688ac7922158e567aef3f4c
kika <kikamozga18@ya.ru>
2021-12-28 15:15:27 +0300

    добавлена первоначальная иницилизация бд

--- Services/DbContex.cs
+++ Services/DbContex.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using MoneyApp.Models;
 
 namespace MoneyApp.Services{
 
@@ -6,12 +7,33 @@ namespace MoneyApp.Services{
     {
         private readonly string _dbPath;
 
+        public DbSet<Wallet> Wallets { get; set; } = default!;
+
         public AppContext(string dbPath){
             _dbPath = dbPath;
         } 
 
-        protected override void  OnConfiguring(DbContextOptionsBuilder optionsBuilder){
-            optionsBuilder.UseSqlite($"DataSource={_dbPath}");
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite($"DataSource={_dbPath}");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Wallet>().HasData(
+                new Wallet[]{
+                    new Wallet(){
+                        Id = 1,
+                        Name = "wallet",
+                        Amount = 15000, 
+                        AmountRatioId = 1
+                    },
+                    new Wallet(){
+                        Id = 2,
+                        Name = "wallet2",
+                        Amount = 1300,
+                        AmountRatioId = 2
+                    }
+                }
+            );
         }
 
     }
