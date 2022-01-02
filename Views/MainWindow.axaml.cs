425cb5fb95787d582e5a5bb064d3ab38d878c050
kika <kikamozga18@ya.ru>
2022-01-02 18:14:37 +0300

    добавлен обработчик и вызов диалог. окна

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,11 +1,15 @@
 using Avalonia;
-using Avalonia.Controls;
 using Avalonia.ReactiveUI;
 using Avalonia.Markup.Xaml;
+using System.Threading.Tasks;
 using ReactiveUI;
+using MoneyApp.Models;
+using MoneyApp.ViewModels;
+using MoneyApp.Dialog;
+
 namespace MoneyApp.Views
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
     {
         public MainWindow()
         {
@@ -13,11 +17,19 @@ namespace MoneyApp.Views
 #if DEBUG
             this.AttachDevTools();
 #endif
+            this.WhenActivated(d => d(ViewModel!.AddWalletInteraction.RegisterHandler(AddWalletOpenDialog)));
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        public async Task AddWalletOpenDialog(InteractionContext<AddWalletViewModel, Wallet?> interaction){
+            var dialog = new DialogWindow();
+            dialog.DataContext = interaction.Input;
+            var result = await dialog.ShowDialog<Wallet?>(this);
+            interaction.SetOutput(result);
         }
     }
 }
