572a9118a821d52ad919df6d2c322e9b5e0b34de
kika <kikamozga18@ya.ru>
2022-01-05 14:58:47 +0300

    добавлена логика

--- ViewModels/AddWalletViewModel.cs
+++ ViewModels/AddWalletViewModel.cs
@@ -1,11 +1,38 @@
-
+using ReactiveUI;
+using MoneyApp.Models;
 
 namespace MoneyApp.ViewModels
 {
     public class AddWalletViewModel : ViewModelBase
     {
+
+        private string? _name;
+        private int _amount;
+
+        public string InputName
+        {
+            get => _name!;
+            set => this.RaiseAndSetIfChanged(ref _name, value);
+        }
+
+        public int InputAmount
+        {
+            get => _amount;
+            set => this.RaiseAndSetIfChanged(ref _amount, value);
+        }
+
+        public IReactiveCommand AddCommand { get; }
+
         public AddWalletViewModel(){
             
+            AddCommand = ReactiveCommand.Create<Wallet>(() => {
+                return new Wallet(){
+                    Name = InputName,
+                    Amount = InputAmount,
+                    AmountRatioId = 1
+                };
+            });
+
         }
     }
 }
