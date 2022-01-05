280658aaf17482e4cb61d2c238c91380e9fe9a7c
kika <kikamozga18@ya.ru>
2022-01-05 19:17:16 +0300

    добавлена подкиска на закрытие окна

--- ViewModels/AddDialogViewModel/AddRecordViewModel.cs
+++ ViewModels/AddDialogViewModel/AddRecordViewModel.cs
@@ -1,8 +1,35 @@
+using ReactiveUI;
+using System.Reactive;
+using MoneyApp.Models;
 
 namespace MoneyApp.ViewModels
 {
     public class  AddRecordViewModel:ViewModelBase
     {
-        
+        private string? _text;
+        private int _amount;
+
+        public string InputText 
+        {
+            get => _text!;
+            set => this.RaiseAndSetIfChanged(ref _text, value);
+        }
+
+        public int InputAmount
+        {
+            get => _amount;
+            set => this.RaiseAndSetIfChanged(ref _amount, value);
+        }
+
+        public ReactiveCommand<Unit, Record> AddCommand { get; }
+
+        public AddRecordViewModel(){
+            AddCommand = ReactiveCommand.Create<Record>(()=>{
+                return new Record(){
+                    Name = InputText,
+                    Amount = InputAmount
+                };
+            });
+        }
     }
 }
